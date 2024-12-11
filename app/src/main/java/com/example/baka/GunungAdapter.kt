6263fea3a10afa3b10245418d3cd2f24b4fdcaa8
walkerzurich <walkerzurich@gmail.com>
2024-12-11 17:56:24 +0700

    Commit prttama

---
+++ app/src/main/java/com/example/baka/GunungAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.baka
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.baka.R
+
+class GunungAdapter(
+    private val context: Context,
+    private val gunungList: List<Gunung>
+) : RecyclerView.Adapter<GunungAdapter.GunungViewHolder>() {
+
+    class GunungViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val ivGunung: ImageView = itemView.findViewById(R.id.ivGunung)
+        val tvNamaGunung: TextView = itemView.findViewById(R.id.tvNamaGunung)
+        val tvLokasiGunung: TextView = itemView.findViewById(R.id.tvLokasiGunung)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GunungViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_gunung, parent, false)
+        return GunungViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: GunungViewHolder, position: Int) {
+        val gunung = gunungList[position]
+        holder.ivGunung.setImageResource(gunung.imageRes)
+        holder.tvNamaGunung.text = gunung.nama
+        holder.tvLokasiGunung.text = gunung.lokasi
+    }
+
+    override fun getItemCount(): Int {
+        return gunungList.size
+    }
+}
+
+data class Gunung(val nama: String, val lokasi: String, val imageRes: Int)
---
+++ app/src/main/java/com/example/baka/transaksiSelesai.kt
@@ -0,0 +1,15 @@
+package com.example.baka
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class transaksiSelesai : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+    }
+}
